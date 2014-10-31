641355bd28d4cb21f3ba886e8025502e5c43ca49
ianchristopherray <iancray@Iastate.edu>
2014-10-31 16:43:18 -0500

    Renamed it MulRFModel

--- treefix-1.1.10/python/treefix/models/mulrfmodel.py
+++ treefix-1.1.10/python/treefix/models/mulrfmodel.py
@@ -16,7 +16,7 @@ from compbio import phylo
 
 #=============================================================================
 
-class DupLossModel(CostModel):
+class MulRFModel(CostModel):
     """Computes Dup/Loss costs"""
 
     def __init__(self, extra):
@@ -41,37 +41,27 @@ class DupLossModel(CostModel):
 
     def optimize_model(self, gtree, stree, gene2species):
         """Optimizes the model"""
-        CostModel.optimize_model(self, gtree, stree, gene2species)
+        # CostModel.optimize_model(self, gtree, stree, gene2species)
 
-        if self.dupcost < 0:
-            self.parser.error("-D/--dupcost must be >= 0")
-        if self.losscost < 0:
-            self.parser.error("-L/--losscost must be >= 0")
+        # if self.dupcost < 0:
+        #     self.parser.error("-D/--dupcost must be >= 0")
+        # if self.losscost < 0:
+        #     self.parser.error("-L/--losscost must be >= 0")
 
-        # ensure gtree and stree are both rooted and binary
-        if not (treelib.is_rooted(gtree) and treelib.is_binary(gtree)):
-            raise Exception("gene tree must be rooted and binary")
-        if not (treelib.is_rooted(stree) and treelib.is_binary(stree)):
-            raise Exception("species tree must be rooted and binary")
-        try:
-            junk = phylo.reconcile(gtree, stree, gene2species)
-        except:
-            raise Exception("problem mapping gene tree to species tree")
+        # # ensure gtree and stree are both rooted and binary
+        # if not (treelib.is_rooted(gtree) and treelib.is_binary(gtree)):
+        #     raise Exception("gene tree must be rooted and binary")
+        # if not (treelib.is_rooted(stree) and treelib.is_binary(stree)):
+        #     raise Exception("species tree must be rooted and binary")
+        # try:
+        #     junk = phylo.reconcile(gtree, stree, gene2species)
+        # except:
+        #     raise Exception("problem mapping gene tree to species tree")
 
     def recon_root(self, gtree, newCopy=True, returnCost=False):
         """Reroots the tree by minimizing the duplication/loss cost"""
-        return phylo.recon_root(gtree, self.stree, self.gene2species,
-                                newCopy = newCopy,
-                                keepName = True, returnCost = returnCost,
-                                dupcost = self.dupcost, losscost = self.losscost)
+        
 
     def compute_cost(self, gtree):
         """Returns the duplication-loss cost"""
-        recon = phylo.reconcile(gtree, self.stree, self.gene2species)
-        events = phylo.label_events(gtree, recon)
-        cost = 0
-        if self.dupcost != 0:
-            cost += phylo.count_dup(gtree, events) * self.dupcost
-        if self.losscost != 0:
-            cost += phylo.count_loss(gtree, self.stree, recon) * self.losscost
-        return cost
+        
